6d2b912d25e38e0e772abc1d40b267025dfad5f7
Vinayak Majgaonkar <43469715+vinayak2709@users.noreply.github.com>
2019-11-16 12:21:35 +0530

    argparse should run thrould command line.

---
+++ argpasre.py
@@ -0,0 +1,8 @@
+import cv2
+import argparse
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+print(args)
