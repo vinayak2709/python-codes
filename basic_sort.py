95541466dd646eb0d2459872b0ea6037c9742907
Vinayak Majgaonkar <43469715+vinayak2709@users.noreply.github.com>
2019-11-21 18:12:00 +0530

    basic_bubble_sort(not for char array,strings)

---
+++ basic_sort.py
@@ -0,0 +1,21 @@
+def sortt(arr):
+    n = len(arr)
+
+    # Traverse through all array elements
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+sample_array=[2,5,7,3,96,4,2,6]
+sorted_array=sortt(sample_array)
+print(sorted_array)
