212b5bf4594dbc9accfc0365f5281b10f4667726
Vinayak Majgaonkar <43469715+vinayak2709@users.noreply.github.com>
2022-05-22 10:47:34 +0530

    argparese code cleaning

--- argpasre.py
+++ argpasre.py
@@ -1,8 +1,7 @@
-import cv2
 import argparse
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
+ap.add_argument("-i", "--value", required=True, help="Path to the image")
 args = vars(ap.parse_args())
 print(args)
